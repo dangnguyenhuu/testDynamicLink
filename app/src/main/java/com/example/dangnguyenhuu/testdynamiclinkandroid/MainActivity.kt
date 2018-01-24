c0ae4ec900145626eb70a3e72f0143e8da63b995
dang nguyenhuu <dangnguyenhuu88@gmail.com>
2018-01-24 12:06:44 +0900

    init project

---
+++ app/src/main/java/com/example/dangnguyenhuu/testdynamiclinkandroid/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dangnguyenhuu.testdynamiclinkandroid
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.util.Log
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        this.webOpenButton.setOnClickListener {
+
+            val builder = CustomTabsIntent.Builder()
+            val tabIntent = builder.setShowTitle(true)
+                    .enableUrlBarHiding().build()
+
+            tabIntent.launchUrl(this, Uri.parse("https://qiita.com/hikariraina/items/d60eb06c7b30e7833133"))
+        }
+
+
+        this.dynamicLinkButton.setOnClickListener {
+
+            val builder = CustomTabsIntent.Builder()
+            val tabIntent = builder.setShowTitle(true)
+                    .enableUrlBarHiding().build()
+
+//            tabIntent.launchUrl(this, Uri.parse("https://c6nwy.app.goo.gl/kCdB"))
+            tabIntent.launchUrl(this, Uri.parse("http://www.example.com/gizmos"))
+        }
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener { test ->
+                    Log.d("Dang  ----> ", "  test")
+
+                }
+    }
+}
